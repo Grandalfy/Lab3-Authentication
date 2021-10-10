9356fe0e25f6b57076064c027f641ac63cf20679
charles <cfb3@uw.edu>
2021-10-10 08:31:50 -0700

    Moved setContentView in onCreate for MainActivity to AFTER the ViewModel creation.

--- app/src/main/java/edu/uw/tcss450/lab3_authentication/MainActivity.java
+++ app/src/main/java/edu/uw/tcss450/lab3_authentication/MainActivity.java
@@ -12,13 +12,14 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
 
         MainActivityArgs args = MainActivityArgs.fromBundle(getIntent().getExtras());
 
         new ViewModelProvider(this,
                 new UserInfoViewModel.UserInfoViewModelFactory(args.getEmail(), args.getJwt())
                 ).get(UserInfoViewModel.class);
+
+        setContentView(R.layout.activity_main);
 
     }
 }
